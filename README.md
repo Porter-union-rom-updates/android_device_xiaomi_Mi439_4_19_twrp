fb7e0bf8504f7bb24401eb2740117da804189d9b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-05 20:38:43 +0200

    Mi439_4_19: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: SDM439 Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Xiaomi SDM439 (Mi439_4_19)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_Mi439_4_19.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Mi439_4_19 device
+$(call inherit-product, device/xiaomi/Mi439_4_19/device.mk)
+
+PRODUCT_DEVICE := Mi439_4_19
+PRODUCT_NAME := omni_Mi439_4_19
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SDM439
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys
